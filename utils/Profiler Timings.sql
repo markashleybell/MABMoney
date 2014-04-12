7c7c19a955ffd65616cacd56b1afb5d54300299f
Mark Bell <me@markashleybell.com>
2014-04-12 10:13:07 +0100

    Add average timing query

--- utils/Profiler Timings.sql
+++ utils/Profiler Timings.sql
@@ -5,17 +5,36 @@ go
 --delete from MiniProfilerTimings
 --delete from MiniProfilers
 
+declare @filter nvarchar(50)
+
+set @filter = 'http://localhost%'
+set @filter = 'X'
+
 select 
 	Name, 
 	SUM(DurationMilliseconds) as TotalDuration 
 from 
 	MiniProfilerTimings
 where 
-	Name not like 'http://localhost%'
+	Name not like @filter
 group by
 	Name
 order by
 	TotalDuration desc
+	
+	
+select 
+	Name, 
+	SUM(DurationMilliseconds) / Count(*) as AverageDuration
+from 
+	MiniProfilerTimings
+where 
+	Name not like @filter
+group by
+	Name
+order by
+	AverageDuration desc
+	
 	
 --select 
 --	Name, 
